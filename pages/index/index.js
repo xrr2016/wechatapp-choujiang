9a08a7c683a2c692044b6cd983a0a37bcbf45940
xrr2016 <13286904669@163.com>
2016-12-09 16:29:10 +0800

    :hankey: edit

--- pages/index/index.js
+++ pages/index/index.js
@@ -20,7 +20,117 @@ Page({
     ]
   },
   onLoad: function () {
+    const self = this,
+          leftPoint = 7.5,
+          topPoint = 7.5,
+          pointList = []
+     for(let i = 0;i<24;i++){
+      if (i == 0) {
+        topPoint = 15;
+        leftPoint = 15;
+      } else if (i < 6) {
+        topPoint = 7.5;
+        leftPonit = leftPoint + 102.5;
+      } else if (i == 6) {
+        topPoint = 15
+        leftPoint = 620;
+      } else if (i < 12) {
+        topPoint = topCircle + 94;
+        leftPoint = 620;
+      } else if (i == 12) {
+        topPoint = 565;
+        leftPoint = 620;
+      } else if (i < 18) {
+        topPoint = 570;
+        leftPoint = leftPoint - 102.5;
+      } else if (i == 18) {
+        topPoint = 565;
+        leftPoint = 15;
+      } else if (i < 24) {
+        topPoint = topPoint - 94;
+        leftPoint = 7.5;
+      } else {
+        return
+      }
+      pointList.push({topPoint:topPoint,leftPoint:leftPoint})
+     }     
+     self.setData({
+       pointList:pointList
+     })
+
+     setInterval(function(){
+       let color1 = self.data.pointColors[0],
+           color2 = self.data.pointColors[1]
+       if(color1 === '#A4F6F9'){
+         self.setData({
+           color1 : '#FF99FE',
+           color2 : '#A4F6F9'
+         })
+       }else{
+         self.setData({
+           color2 : '#FF99FE',
+           color1 : '#A4F6F9'
+         })
+       }
+     },800)
+     const prizeList = []
+     let topPrize = 25,
+         leftPrize = 25
+    for(let i = 0;i < 8;i++){
+      if(i === 0){
+        topPrize = 25
+        leftPrize = 25
+      }else if(i < 3){
+        topPrize = topPrize
+        leftPrize = leftPrize + 166.666 + 15
+      }else if(i < 5){
+        leftPrize = leftPrize
+        topPrize = topPrize + 150 + 15
+      }else if(i < 7){
+        leftPrize = leftPrize - 166.666 -15
+        topPrize = topPrize
+      }else if (i < 8 ){
+        leftPrize = leftPrize
+        topPrize = topPrize - 150 -15
+      }
+      let prizeImg = self.data.prizeImgs[i]
+      prizeList.push({topPrize:topPrize,leftPrize:leftPrize,prizeImg:prizeImg})
+    }
+    self.setData({
+      prizeList : prizeList
+    })
+  },
+  startGame(){
+    if(this.data.isRunning === true) return 
+    this.setData({
+      isRunning:true
+    })
     const self = this
-    
+    let prizeIndex = 0
+    let i = 0
+    const timer = setInterval(function(){
+      prizeIndex++
+      i += 30
+      if(i >= 1000){
+        clearInterval(timer)
+
+        wx.showModal({
+          title:'恭喜你',
+          content:`获得了第${self.data.peizeIndex + 1}个奖品`,
+          showCancel:false,
+          success:function(res){
+            if(res.confirm){
+              self.setData({
+                isRunning:false
+              })
+            }
+          }
+        })
+        prizeIndex = prizeIndex % 8 
+        self.setData({
+          prizeIndex:prizeIndex
+        })
+      }
+    },(200 + i))
   }
 })
